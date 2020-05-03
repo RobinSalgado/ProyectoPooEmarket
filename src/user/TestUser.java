06832dcd5572d873016408d80b6962e6b2851d39
RobinSalgado <ie686481@iteso.mx>
2020-05-02 20:57:06 -0500

    commit para hacer py

--- src/user/TestUser.java
+++ src/user/TestUser.java
@@ -6,19 +6,11 @@ import util.Session;
 public class TestUser {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		
-		initializeUsers u = new initializeUsers();
-		Customer Cstmr = new Customer();
-		Cstmr.signUp();
-		System.out.println(u.users.keySet());
-		Cstmr.logIn();
 
-=======
 		Session session = new Session();
 		session.signUp();
 		session.logIn();
->>>>>>> 855a8f0533c71636859f5e30a2e53f1d78c1d20c
+
 	}
 
 }
